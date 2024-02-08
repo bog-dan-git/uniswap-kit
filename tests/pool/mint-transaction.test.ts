db9d7ebbd3a07d75c85f9e4d2cfaed498db8281d
bog-dan-git <43274641+bog-dan-git@users.noreply.github.com>
2024-02-09 01:57:10 +0200

    tests: mint approvals

--- tests/pool/mint-transaction.test.ts
+++ tests/pool/mint-transaction.test.ts
@@ -3,9 +3,11 @@ import { FeeAmount } from '@uniswap/v3-sdk';
 import { RPC_URL, WALLET_ADDRESS, WALLET_KEY, TOKEN0_ADDRESS, TOKEN1_ADDRESS } from '../utils';
 import { transactionMining } from '../utils/web3';
 import { PositionManager } from '../../src/positions/position-manager';
+import { ERC20Facade } from '../../src/erc20';
 
 describe('Mint tests', () => {
   let pool!: UniswapPool;
+  let erc20Facade!: ERC20Facade;
 
   beforeAll(async () => {
     pool = await UniswapPool.fromTokens(RPC_URL, {
@@ -13,6 +15,7 @@ describe('Mint tests', () => {
       token2Address: TOKEN0_ADDRESS,
       fee: FeeAmount.MEDIUM,
     });
+    erc20Facade = new ERC20Facade(RPC_URL);
 
     const activePositions = await pool.getActivePositions(WALLET_ADDRESS);
 
@@ -122,6 +125,72 @@ describe('Mint tests', () => {
 
       await transactionMining(RPC_URL, burnTx.transactionHash.toString());
     });
+  });
+
+  it('Should mint with approval', async () => {
+    const reset0 = await erc20Facade
+      .approve(TOKEN0_ADDRESS, pool.config.deploymentAddresses.nonFungiblePositionManager, 0n)
+      .then((x) => x.execute({ rpcUrl: RPC_URL, privateKey: WALLET_KEY }));
+    const reset1 = await erc20Facade
+      .approve(TOKEN1_ADDRESS, pool.config.deploymentAddresses.nonFungiblePositionManager, 0n)
+      .then((x) => x.execute({ rpcUrl: RPC_URL, privateKey: WALLET_KEY }));
+
+    await transactionMining(RPC_URL, reset0.transactionHash.toString());
+    await transactionMining(RPC_URL, reset1.transactionHash.toString());
+
+    const transaction = await pool
+      .createMintTransaction()
+      .fromPercents({
+        percentLower: {
+          numerator: 1,
+          denominator: 10,
+        },
+        percentUpper: {
+          numerator: 1,
+          denominator: 10,
+        },
+      })
+      .fromAmount0(10n ** 6n)
+      .verifyAllowance(WALLET_ADDRESS)
+      .buildTransaction({
+        recipient: WALLET_ADDRESS,
+      });
+
+    await transaction.execute({
+      rpcUrl: RPC_URL,
+      privateKey: WALLET_KEY,
+    });
+
+    const activePositions = await pool.getActivePositions(WALLET_ADDRESS);
+    expect(activePositions.length).toBe(1);
+
+    const position = activePositions[0];
+
+    expect(position.liquidity).toBeGreaterThan(0n);
+
+    const allowance0 = await erc20Facade.allowance(
+      TOKEN0_ADDRESS,
+      WALLET_ADDRESS,
+      pool.config.deploymentAddresses.nonFungiblePositionManager,
+    );
+    const allowance1 = await erc20Facade.allowance(
+      TOKEN1_ADDRESS,
+      WALLET_ADDRESS,
+      pool.config.deploymentAddresses.nonFungiblePositionManager,
+    );
+
+    expect(allowance0).toBe(0n);
+    expect(allowance1).toBe(0n);
+
+    const positionManager = await PositionManager.create(RPC_URL);
+    const burnTx = await positionManager.closeAndBurnPosition(position, WALLET_ADDRESS).then((x) =>
+      x.execute({
+        rpcUrl: RPC_URL,
+        privateKey: WALLET_KEY,
+      }),
+    );
+
+    await transactionMining(RPC_URL, burnTx.transactionHash.toString());
   });
   it('Should get pool token addresses', () => {});
   it('Should get token0 price', () => {});
